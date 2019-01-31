aa150fea133eeff549ac01db3603f2caa827ad08
Aлександр Назаров <aleksandr.nazarov@vokigames.com>
2019-01-31 09:26:37 +0200

    updated timingsfix, modified dir walk logic

--- renamer.py
+++ renamer.py
@@ -1,31 +1,44 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+"""
+Author: Oleksandr Nazarov - aleksandr.nazarov@vokigames.com
+
+Script to walk dirs and subdirs recursively and rename certain files with to a certain pattern
+v0.2
+
+to do:
+	*make it universal. pass as arguments regex and dst name (what about extensions?)
+
+	*change extension? pass new extension as argument
+
+"""
+
 import sys
 sys.dont_write_bytecode = True
 
 import os
 import os.path
 import argparse
+from shutil import move
+
 
 def main(dirName): 
 	preset = 'timing.txt'
-	print "Dir name: " + dirName
+	# print "Dir name: " + dirName
 
 	for dirpath, dirnames, filenames in os.walk(dirName):
 		for filename in [f for f in filenames if f.endswith(".txt")]:
-			if filename == preset:
-				pass
-			else:
-				print "Dir: " + dirpath
-				print "Found: " + filename
-				os.rename(dirpath + "/" + filename, dirpath + "/" + preset)
-				if not filename.endswith( '.txt' ):
-					filename += '.txt'
+			print "Dir: " + dirpath
+			print "Found: " + filename
+			move(dirpath + "/" + filename, dirpath + "/" + preset)
+			if not filename.endswith( '.txt' ):
+				filename += '.txt'
+
 
 if __name__ == '__main__': 
     
-    print "TXT timings file renamer script - v0.1"
+    print "TXT timings file renamer script - v0.2"
     parser = argparse.ArgumentParser()
     parser.add_argument("dirName", help = "dir name to look for timings")
     args = parser.parse_args()
